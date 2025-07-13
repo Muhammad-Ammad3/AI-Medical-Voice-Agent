863b6292a77c13f53444c7c06fb1502d9341775a
Ammad <ammad0598@gmail.com>
2025-07-13 07:53:25 +0500

    first commit

--- app/(routes)/dashboard/_components/SuggestedDoctorCard.tsx
+++ app/(routes)/dashboard/_components/SuggestedDoctorCard.tsx
@@ -4,12 +4,13 @@ import Image from "next/image";
 
 type props = {
   doctorAgent: doctorAgent;
-  setSelectedDoctor: any
+  setSelectedDoctor: any;
+  selectedDoctor: doctorAgent;
 
 };
-function SuggestedDoctorCard({ doctorAgent,setSelectedDoctor }: props) {
+function SuggestedDoctorCard({ doctorAgent,setSelectedDoctor,selectedDoctor }: props) {
   return (
-    <div className={`flex flex-col items-center border rounded-2xl shadow p-5 hover:border-blue-500 cursor-pointer ${setSelectedDoctor?.id == doctorAgent?.id && "border-blue-500"}`} onClick={() => setSelectedDoctor(doctorAgent)}>
+    <div className={`flex flex-col items-center border rounded-2xl shadow p-5 hover:border-blue-500 cursor-pointer ${selectedDoctor?.id == doctorAgent?.id && "border-blue-500"}`} onClick={() => setSelectedDoctor(doctorAgent)}>
       <Image
         src={doctorAgent?.image}
         alt={doctorAgent?.specialist}
