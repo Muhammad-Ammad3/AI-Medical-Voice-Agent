3792815839aafbbb75949867687657db51867467
Ammad <ammad0598@gmail.com>
2026-01-09 00:38:53 +0500

    updates

--- app/(routes)/dashboard/medical-agent/[sessionId]/page.tsx
+++ app/(routes)/dashboard/medical-agent/[sessionId]/page.tsx
@@ -226,7 +226,12 @@ function MedicalVoiceAgent() {
   const {sessionId} = useParams()
   const [sessionDetails, setSessionDetails] = useState<SessionDetail>()
   const [callStarted, setCallStarted] = useState(false);
-  const vapi = new Vapi(process.env.NEXT_PUBLIC_VAPI_API_KEY!);
+  const [vapiInstance, setVapiInstance] = useState<any>();
+  const [currentRole, setCurrentRole] = useState<string | null>();
+  const [liveTranscript, setLiveTranscript] = useState<string>();
+  const [message, setMessage] = useState<messages[]>([]);
+
+
   useEffect(() => {
     sessionId && GetSessionDetails();
   },[sessionId]);
@@ -238,6 +243,8 @@ function MedicalVoiceAgent() {
   }
 
   const StartCall = () => {
+  const vapi = new Vapi(process.env.NEXT_PUBLIC_VAPI_API_KEY!);
+  setVapiInstance(vapi);
   vapi.start(process.env.NEXT_PUBLIC_VAPI_VOICE_ASSISTANT_ID!);
   vapi.on('call-start', () => {console.log('Call started')
     setCallStarted(true);
@@ -247,10 +254,39 @@ vapi.on('call-end', () => {console.log('Call ended')
 });
 vapi.on('message', (message) => {
   if (message.type === 'transcript') {
+    const { role, transcript, transcriptType } = message;
     console.log(`${message.role}: ${message.transcript}`);
+    if(transcriptType == 'partial') {
+    setLiveTranscript(transcript);
+    setCurrentRole(role);
+    }
+    else if(transcriptType == 'final') {
+      setMessage((prev: any) => [...prev, {role: role, text: transcript}]);
+      setLiveTranscript("");
+      setCurrentRole(null);
+    }
   }
 });
+ vapiInstance.on('speech-start', () => {
+      console.log('Assistant started speaking');
+      setCurrentRole("assistant");
+    });
+    vapiInstance.on('speech-end', () => {
+      console.log('Assistant stopped speaking');
+      setCurrentRole("user");
+    });
   }
+    const endCall = () => {
+    if (!vapiInstance) return;
+      vapiInstance.stop();
+      vapiInstance.off('call-start');
+      vapiInstance.off('call-end');
+      vapiInstance.off('message');
+      setCallStarted(false);
+      setVapiInstance(null);
+    
+  };
+
 
   return (
     <div className="p-5 border rounded-3xl bg-secondary">
@@ -258,11 +294,11 @@ vapi.on('message', (message) => {
         <h2 className="p-1 px-2 border rounded-md flex gap-2 items-center">
           {" "}
           <Circle
-            // className={`h-4 w-4 rounded-full ${
-            //   callStarted ? "bg-green-500" : "bg-red-500"
-            // }`}
+            className={`h-4 w-4 rounded-full ${
+              callStarted ? "bg-green-500" : "bg-red-500"
+            }`}
           />
-          {/* {callStarted ? "Connected..." : "Not Connected"} */}
+          {callStarted ? "Connected..." : "Not Connected"}
         </h2>
         <h2 className="font-bold text-xl text-gray-400">00:00</h2>
       </div>
@@ -279,19 +315,20 @@ vapi.on('message', (message) => {
             {sessionDetails?.selectedDoctor?.specialist}
           </h2>
           <p className="text-sm text-gray-400">AI Medical Voice Agent</p>
-          <div className="mt-32 overflow-y-auto">
-            {/* {message?.map((msg: messages, index) => (
-              <h2 className="text-gray-400" key={index}>
+          <div className="mt-12 overflow-y-auto flex flex-col items-center px-10 md:px-28 lg:px-52 xl:px-72">
+         
+            {message?.slice(-4).map((msg: messages, index) => (
+              <h2 className="text-gray-400 p-2" key={index}>
                 {msg.role} : {msg.text}
               </h2>
-            ))} */}
-            {/* {liveTranscript && liveTranscript?.length > 0 && (
+            ))}
+            {liveTranscript && liveTranscript?.length > 0 && (
               <h2 className="text-lg">
-                {currentRoll} : {liveTranscript}
+                {currentRole} : {liveTranscript}
               </h2>
-            )} */}
+            )}
           </div>
-          {/* {!callStarted ? (
+          {!callStarted ? (
             <Button className="mt-20" onClick={StartCall}>
               {" "}
               <PhoneCall /> Start Call
@@ -300,7 +337,7 @@ vapi.on('message', (message) => {
             <Button variant="destructive" onClick={endCall}>
               <PhoneOff /> Disconnect
             </Button>
-          )} */}
+          )}
         </div>
       )}
     </div>
